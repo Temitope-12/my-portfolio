460ae08492de01f3262859034023a707d8985b8e
unknown <ibikunletemmytee@gmail.com>
2022-08-21 15:22:33 +0100

    added javascript alert

---
+++ index.js
@@ -0,0 +1,8 @@
+// document.getElementById('firstdiv')
+var second=document.getElementById('firstdiv')
+
+
+second.addEventListener('click',function() {alert('welcome,i am purple')
+    
+})
+second.style.background="yellow"
